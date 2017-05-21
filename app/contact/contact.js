4aedda4d18593c490b61917b44486573de2e4f0f
Sam Maphey <sammaphey@gmail.com>
2017-05-21 13:20:37 -0400

    Added google Maps and more

---
+++ app/contact/contact.js
@@ -0,0 +1,10 @@
+app.controller("contact", function($scope, NgMap) {
+
+
+    NgMap.getMap().then(function(map) {
+      console.log(map.getCenter());
+      console.log('markers', map.markers);
+      console.log('shapes', map.shapes);
+    });
+
+});
