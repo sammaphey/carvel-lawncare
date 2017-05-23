1c0422c92169f0d572344dc67e0c1d6ab3cbc3f2
Sam Maphey <sammaphey@gmail.com>
2017-05-23 11:49:19 -0400

    Added more specifics

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,3 @@
-var app = angular.module('angularApp', ['ngRoute', 'ngAnimate', 'ngMap']);
+var app = angular.module('angularApp', ['ngRoute', 'ngAnimate']).config(function($locationProvider) {
+  // $locationProvider.hashPrefix('');
+});
--- app/services/services.js
+++
@@ -1,2 +0,0 @@
-app.controller("service", function($scope) {
-});
